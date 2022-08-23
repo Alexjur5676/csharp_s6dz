c25068db910d9711ea30f5b4d7624e42635d778f
Alexjur5676 <alex9992142@mail.ru>
2022-08-23 16:35:57 +0700

    Add task_41_43

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,4 +1,44 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+// // 1, -7, 567, 89, 223-> 3
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int index = 0; index < length; index++)
+    {
+        Console.WriteLine($"Введите число M{index + 1}:");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        array[index] = Number;
+    }
+}
+void PrintArray(int[] Array)
+
+{
+    int count = Array.Length;
+    for (int index = 0; index < count; index++)
+    {
+       Console.Write(Array[index] + " ");
+    }
+}
+Console.WriteLine("Какое колличество чисел M будет введено? ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+int count = array.Length;
+FillArray(array);
+Console.WriteLine("Введенные числа: ");
+PrintArray(array);
+
+int sum = 0;
+for (int i = 0; i < M; i++)
+{
+    if (array[i] > 0)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел {sum} ");
+
+
--- task43/Program.cs
+++ task43/Program.cs
@@ -3,3 +3,33 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+// x = (d-c)/(a-b)
+// y=a*(d-c)/(a-b)+c
+
+Console.WriteLine("значения b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("значения k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("значения b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("значения k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+if (b2 == b1 && k1 == k2)
+    {
+        Console.WriteLine("Прямые совпадают ");
+    }
+else
+    if(k1 == k2)
+    {
+        Console.WriteLine("Прямые паралельны ");
+    }
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Прямые пересекаются в точке с координатами: X = {x} , Y = {y} ");
+}
+
+
